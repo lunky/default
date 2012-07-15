5ef203bb7b8f8758923818391318cb797662df47
libra guest <c9@midworld.org>
2012-07-14 22:49:12 -0400

    current

--- js/script.js
+++ js/script.js
@@ -1,27 +1,23 @@
 /* Author: Quinn Wilson
 
 */
-var Workout;
-Workout = Ember.Object.extend({
-  startWorkout: function(thing) {
-    alert(thing);
- }
+
+Workouts = Ember.Application.create();
+
+Workouts.Workout = Ember.Object.extend({
+    date: new Date(),
+    name: "default",
+    save: function(){
+//      alert("save'n " + this.get('name') + ' created ' + this.get('date') );
+    }
 });
 
-<<<<<<< HEAD
-var Workout = Em.Object.extend({
-    date: Date.now(),
-=======
-var wk = Workout.create();
-wk.startWorkout("Hello") // alerts "Hello"
->>>>>>> 71be1a497a44fee70e1ef6f1e07803e170a23405
-
-});
-
-var workout = Workout.create({
-  name: "default",
-  save: function(){
-      alert("save'n");
-  }
+var workout = Workouts.Workout.create({
+    name: "front squat"
 });
 workout.save();
+
+var view = Ember.View.create({
+  color : "Bob"
+});
+view.append("#werkouts");
